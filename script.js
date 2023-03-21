c00bc55e7cf3eca4a49f503c79d8a7647c8562a0
Pam_Tenório <pamellahellenaa@gmail.com>
2023-03-20 23:06:17 -0300

    primeiro commit- projeto iphone

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+function trocaCor(cor ){
+   let circulo = document.querySelector(".circulo")
+   circulo.style.background = cor
+}
+function trocaimagem (imagem){
+    let imgiPhone = document.querySelector(".iphone")
+    imgiPhone.src = imagem 
+}
